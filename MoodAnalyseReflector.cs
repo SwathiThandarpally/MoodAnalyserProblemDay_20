00a33594038d7ca01c11a2619486fa51c98132d7
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-11-10 18:22:56 +0530

    Added mood analysereflector program

---
+++ MoodAnalyseReflector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_20MoodAnalyser
+{
+    public class MoodAnalyseReflector
+    {
+        public MoodAnalyseReflector()
+        {
+            Console.WriteLine("Happy Mood");
+        }
+    }
+}
