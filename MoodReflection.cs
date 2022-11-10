a5567787b2f8babbbaaddb9afdb11d93dd712233
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-11-10 18:20:33 +0530

    Added mood reflection program

---
+++ MoodReflection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_20MoodAnalyser
+{
+    public class MoodReflection
+    {
+        public MoodReflection()
+        {
+            
+        }
+        public MoodReflection(string Sad)
+        {
+
+        }
+        public MoodReflection(string Sad, string Happy)
+        {
+
+
+        }
+
+
+    }
+}
