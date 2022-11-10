6f7d68eaaaa27638da588ebe950c661a17433ca7
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-11-10 18:25:59 +0530

    Added mood change reflector program

---
+++ MoodChangeReflector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_20MoodAnalyser
+{
+    public class MoodChangeReflector
+    {
+        public MoodChangeReflector()
+        {
+        }
+
+        public MoodChangeReflector(string Happy)
+        {
+            if(Happy == null)
+            {
+                try
+                {
+                    Console.WriteLine("Happy");
+                }
+                catch
+                {
+                    Console.WriteLine("Sad");
+                }
+            }
+          
+
+        }
+    }
+}
