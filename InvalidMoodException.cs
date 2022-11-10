f4652ffc3184c4c054825189129ed710d19534ef
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-11-10 18:07:53 +0530

    Added invalid mood program

---
+++ InvalidMoodException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_20MoodAnalyser
+{
+    public class InvalidMoodException:Exception 
+    {
+        public InvalidMoodException(string message) : base(message)
+        {
+            message = string.Empty;
+            
+        }
+    }
+}
