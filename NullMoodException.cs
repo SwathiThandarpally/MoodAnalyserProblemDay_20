73ccba881daf18f572e34d59266b4f0fc38a3956
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-11-10 18:02:22 +0530

    Added nullmoodException program

---
+++ NullMoodException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_20MoodAnalyser
+{
+    public class NullMoodException:Exception 
+    {
+        public NullMoodException(string message) : base(message)
+        {
+
+        }
+    }
+}
